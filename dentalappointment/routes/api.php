7d41e5ad00a4ecfd0ee21c2f7358231591562c1f
Christian <chsanleo@gmail.com>
2020-08-03 00:53:46 +0200

    Create routes

--- dentalappointment/routes/api.php
+++ dentalappointment/routes/api.php
@@ -17,3 +17,36 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('auth')->group(function(){
+    Route::post('signup','AuthController@signup');
+    Route::post('login','AuthController@login');
+});
+//CRUD User
+Route::prefix('user')->group(function ()
+{
+    Route::get('{id}', 'UserController@getProfile');
+    Route::post('', 'UserController@createUser');
+    Route::put('{id}', 'UserController@updateUser');
+    Route::delete('{id}', 'UserController@deleteUser');
+});
+
+//CRUD MEDICOS
+Route::prefix('doctor')->group(function ()
+{
+    Route::get('{id}', 'DoctorController@getProfile');
+    Route::post('', 'DoctorController@createDoctor');
+    Route::put('{id}', 'DoctorController@updateDoctor');
+    Route::delete('{id}', 'DoctorController@deleteDoctor');
+});
+
+//CITAS
+Route::prefix('appointment')->group(function ()
+{
+    Route::get('{id}', 'AppointmentController@getAllUser');
+    Route::get('{id}', 'AppointmentController@getAllDoctor');
+    Route::post('', 'AppointmentController@createAppointment');
+    Route::put('{id}', 'AppointmentController@updateAppointment');
+    Route::delete('{id}', 'AppointmentController@deleteAppointment');
+});
