4129fa0b2c5abbac3abf8f95ca94f04854746eca
Christian <chsanleo@gmail.com>
2020-08-03 19:28:15 +0200

    Add softdeletes and relationship

--- dentalappointment/app/Appointment.php
+++ dentalappointment/app/Appointment.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Appointment extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['numExp', 'user_id', 'dateAppointment'];
+
+    public function client()
+    {
+        return $this->belongTo('\App\User','user_id','id','users');
+    }
 }
