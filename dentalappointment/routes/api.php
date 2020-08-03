ebcd5f3de87279dcaa9b0d668402e8b0079bc291
Christian <chsanleo@gmail.com>
2020-08-03 19:13:29 +0200

    clean code

--- dentalappointment/routes/api.php
+++ dentalappointment/routes/api.php
@@ -3,29 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::prefix('auth')->group(function(){
-    Route::post('signup','AuthController@signup');
-    Route::post('login','AuthController@login');
+Route::prefix('auth')->group(function () {
+    Route::post('signup', 'AuthController@signup');
+    Route::post('login', 'AuthController@login');
 });
 //CRUD User
-Route::prefix('user')->group(function ()
-{
+Route::prefix('user')->group(function () {
     Route::get('{id}', 'UserController@getProfile');
     Route::post('', 'UserController@createUser');
     Route::put('{id}', 'UserController@updateUser');
@@ -33,8 +21,7 @@ Route::prefix('user')->group(function ()
 });
 
 //CRUD MEDICOS
-Route::prefix('doctor')->group(function ()
-{
+Route::prefix('doctor')->group(function () {
     Route::get('{id}', 'DoctorController@getProfile');
     Route::post('', 'DoctorController@createDoctor');
     Route::put('{id}', 'DoctorController@updateDoctor');
@@ -42,10 +29,9 @@ Route::prefix('doctor')->group(function ()
 });
 
 //CITAS
-Route::prefix('appointment')->group(function ()
-{
-    Route::get('{id}', 'AppointmentController@getAllUser');
-    Route::get('{id}', 'AppointmentController@getAllDoctor');
+Route::prefix('appointment')->group(function () {
+    Route::get('', 'AppointmentController@getAll');
+    Route::get('{id}', 'AppointmentController@get');
     Route::post('', 'AppointmentController@createAppointment');
     Route::put('{id}', 'AppointmentController@updateAppointment');
     Route::delete('{id}', 'AppointmentController@deleteAppointment');
