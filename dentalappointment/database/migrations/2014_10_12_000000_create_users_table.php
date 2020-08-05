8549853628cc677b8a5dec64a4046ca4568f766c
Christian <chsanleo@gmail.com>
2020-08-05 19:46:41 +0200

    Create types
    client predefined and doctor

--- dentalappointment/database/migrations/2014_10_12_000000_create_users_table.php
+++ dentalappointment/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('numExp');
+            $table->enum('type',['client','doctor']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
