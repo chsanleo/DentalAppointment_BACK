c361c5337843787713e771acc71d5a846180ce3b
Christian <chsanleo@gmail.com>
2020-08-06 02:49:05 +0200

    Create methods controller to contactMail

---
+++ dentalappointment/app/Http/Controllers/ContactMailController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\contactMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ContactMailController extends Controller
+{
+    //Route::get('','ContactMailController@getAll');
+    public function getAll()
+    {
+        return ContactMail::get();
+    }
+    //Route::get('{id}','ContactMailController@get');
+    public function get($id)
+    {
+        return ContactMail::find($id);
+    }
+
+    //Route::post('','ContactMailController@createContactMail');
+    public function createContactMail(Request $request, $id)
+    {
+        $body = $request->all();
+        $validator = Validator::make($request->all(), [
+            'contactMail' => 'required|string|max:255',
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string|max:255'
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['message' => 'There was a problem trying to update the user'], 400);
+        }
+        
+        return ContactMail::create($body);
+    }
+
+    //Route::get('{id}', 'AppointmentController@update');
+    public function update($id)
+    {
+        $contactMail = ContactMail::find($id);
+        $contactMail->processed = true;
+        return $contactMail->update($contactMail);
+    }
+}
