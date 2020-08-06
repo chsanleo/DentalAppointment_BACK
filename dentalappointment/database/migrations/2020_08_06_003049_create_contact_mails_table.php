caa11d5987c7cc0f7e9a69f87f3b67c03edb6f92
Christian <chsanleo@gmail.com>
2020-08-06 02:48:42 +0200

    Create migration to ContactMail

---
+++ dentalappointment/database/migrations/2020_08_06_003049_create_contact_mails_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactMailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_mails', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('contactMail');
+            $table->string('user_id');
+            $table->string('subject');
+            $table->string('message');
+            $table->boolean('processed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_mails');
+    }
+}
