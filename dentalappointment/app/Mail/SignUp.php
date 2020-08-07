174a4b339cda75179d11613b6e44f9f9b5b61d4c
Christian <chsanleo@gmail.com>
2020-08-08 00:38:23 +0200

    Create SignUp mail controller

---
+++ dentalappointment/app/Mail/SignUp.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SignUp extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $password;
+    private $numExp;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($numExp, $password)
+    {
+        $this->password = $password;
+        $this->$numExp = $numExp;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.signupMail');
+    }
+}
