058b290d6ee654fa58262afdf6f798727a02a25a
Christian <chsanleo@gmail.com>
2020-08-09 18:14:15 +0200

    Add more random numbers to pass and numExp

--- dentalappointment/app/Http/Controllers/AuthController.php
+++ dentalappointment/app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
     private function ramdomNum()
     {
         $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
-        for ($i = 0; $i < 8; $i++) {
+        for ($i = 0; $i < 13; $i++) {
             $n = rand(0, strlen($alphabet) - 1);
             $ramdomNum[$i] = $alphabet[$n];
         }
